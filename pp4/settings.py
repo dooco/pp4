747abec3ec9732c04ad7e2f83965836cc7888914
dooco <padraigdooley@gmail.com>
2023-01-07 08:00:12 +0000

    add sign up

--- pp4/settings.py
+++ pp4/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'pp4.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'PP4/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
