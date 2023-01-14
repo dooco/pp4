df85b4c6bc0ab389b83d59c0417159d0db0f0101
dooco <padraigdooley@gmail.com>
2023-01-14 22:04:56 +0000

    copy allauth directory account to templates

--- pp4/settings.py
+++ pp4/settings.py
@@ -202,3 +202,4 @@ SOCIALACCOUNT_PROVIDERS = {
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+ACCOUNT_EMAIL_VERIFICATION = 'none'
