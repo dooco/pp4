73efba32fb1b2f50019386b2403b9690187d6118
dooco <padraigdooley@gmail.com>
2023-01-15 15:26:17 +0000

    make app - joinus to hold copy allauth directory

--- pp4/settings.py
+++ pp4/settings.py
@@ -18,7 +18,7 @@ if os.path.isfile("env.py"):
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
+TEMPLATES_DIR = os.path.join(BASE_DIR, 'allauth', 'templates')
 
 
 # Quick-start development settings - unsuitable for production
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     'bootstrap4',
     'cloudinary',
     'accounts',
+    'joinus',
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
