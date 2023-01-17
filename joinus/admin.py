6c26cc30302f777920e6ae705c516df34235e565
dooco <padraigdooley@gmail.com>
2023-01-17 22:12:39 +0000

    add Board_feature and Review models and added edditing on admin

--- joinus/admin.py
+++ joinus/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Board_feature
+from .models import Review
+from django_summernote.admin import SummernoteModelAdmin
 
-# Register your models here.
+
+@admin.register(Board_feature)
+class Board_featureAdmin(SummernoteModelAdmin):
+    list_display = ('board_name', 'slug', 'status', 'created_on')
+    search_fields = ['manufacturer', 'special_features']
+    prepopulated_fields = {'slug': ('board_name',)}
+    list_filter = ('status', 'created_on')
+    summernote_fields = ('special_features')
+
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ('name', 'body', 'post','created_on','approved')
+    list_filter = ('approved', 'created_on')
+    search_fields = ['name', 'email', 'body']
--- joinus/models.py
+++ joinus/models.py
@@ -1,3 +1,51 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+STATUS = ((0, "Draft"), (1, "Published"))
+
+
+class Board_feature(models.Model):
+    slug = models.SlugField(max_length=200, unique=True)
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="feature_post")
+    update_on = models.DateTimeField(auto_now=True)
+    board_name = models.CharField(max_length=255, unique=True)
+    manufacturer = models.CharField(max_length=255, blank=True)
+    memory = models.CharField(max_length=80, blank=True)
+    power = models.CharField(max_length=255, blank=True)
+    clock_frequency = models.CharField(max_length=255, blank=True)
+    conectivity = models.CharField(max_length=255, blank=True)
+    special_features = models.TextField(blank=True)
+    featured_image = CloudinaryField('image', default='placeholder')
+    excerpt = models.TextField(blank=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+    io_pin_number = models.IntegerField(default=0)
+    excerpt = models.TextField(blank=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+    avg_rating = models.ManyToManyField(User, related_name='feature_rating', blank=True)
+
+    class Meta:
+        ordering = ['-created_on']
+
+    def __str__(self):
+        return self.title
+
+    def number_of_ratings(self):
+        return self.avg_rating.count()
+
+
+class Review(models.Model):
+
+    post = models.ForeignKey(Board_feature, on_delete=models.CASCADE, related_name='comments')
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    approved = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ['created_on']
+
+    def __str__(self):
+        return f"Comment {self.body} by {self.name}"
