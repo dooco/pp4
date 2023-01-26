00b50f3da76a2e6302765b7c0c6eee55de50af97
dooco <padraigdooley@gmail.com>
2023-01-26 20:54:25 +0000

    add admin.siteurls to joinus urls.py

--- joinus/urls.py
+++ joinus/urls.py
@@ -2,6 +2,8 @@ from importlib import import_module
 
 from django.urls import include, path
 
+from django.contrib import admin
+
 from allauth.socialaccount import providers
 
 from . import app_settings
@@ -10,6 +12,7 @@ from . import views
 
 urlpatterns = [
     path('', views.Board_featureList.as_view(), name='home'),
+    path('admin/', admin.site.urls),
     path('<slug:slug>/', views.Board_Detail.as_view(), name='board_detail'),
     path('rate/<slug:slug>', views.BoardLike.as_view(), name='board_like'),
     # path("", include("allauth.account.urls"))
--- pp4/urls.py
+++ pp4/urls.py
@@ -13,7 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
+# from django.contrib import admin
 from django.urls import path, include
 from accounts import views
 from django.views.generic import TemplateView
@@ -23,8 +23,7 @@ urlpatterns = [
     # path('', views.login, name='login'),
     # path('signup/', views.signup, name='signup'),
     path('', include('joinus.urls'), name='joinus'),
-    path('admin/', admin.site.urls),
-    # path('joinus/', include('joinus.urls')),
+    # path('admin/', admin.site.urls),
     # path('product_review/', include('product_review/product_review.urls')),
     path('accounts/', include('allauth.urls')),
     path('summernote/', include('django_summernote.urls')),
