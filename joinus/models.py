2462b2c7061b1a16f7b6af05ecd8906566975f6f
dooco <padraigdooley@gmail.com>
2023-02-05 12:36:58 +0000

    add js script to base.html to monitor star rating selection.

--- joinus/models.py
+++ joinus/models.py
@@ -7,31 +7,25 @@ STATUS = ((0, "Draft"), (1, "Published"))
 
 
 class Board_feature(models.Model):
+    board_name = models.CharField(max_length=255, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
     author = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name='board_feature')
     update_on = models.DateTimeField(auto_now=True)
-    board_name = models.CharField(max_length=255, unique=True)
     manufacturer = models.CharField(max_length=255, blank=True)
-    microprocessor = models.CharField(max_length=255, blank=True)
-    memory = models.CharField(max_length=80, blank=True)
-    power = models.CharField(max_length=255, blank=True)
-    clock_frequency = models.CharField(max_length=255, blank=True)
-    conectivity = models.CharField(max_length=255, blank=True)
     special_features = models.CharField(max_length=500, blank=True)
     featured_image = CloudinaryField('image', default='placeholder')
     excerpt = models.CharField(max_length=255, unique=True)
     created_on = models.DateTimeField(auto_now_add=True)
-    io_pin_number = models.IntegerField(default=0)
     status = models.IntegerField(choices=STATUS, default=0)
     avg_rating = models.PositiveIntegerField(default=0)
 
     class Meta:
         ordering = ['-created_on']
 
-    def average_rating(self):
+    def average_rating(self) -> float:
         return Review.objects.filter(board=self).aggregate(
-            Avg("score"))["score__avg"] or 0
+            Avg('score'))['score__avg'] or 0
 
     def __str__(self):
         return f"{self.board_name}: {self.average_rating()}"
