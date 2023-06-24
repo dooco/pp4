807bd1a55c45c830a0f4e0bb055d54481317f9de
Padraig Dooley <padraigdooley@gmail.com>
2023-06-24 07:57:10 +0100

    backup data in date.json

--- pp4/urls.py
+++ pp4/urls.py
@@ -28,7 +28,6 @@ from .views import handler500
 urlpatterns = [
     path("accounts/", include("allauth.urls")),
     path('', include('joinus.urls'), name='joinus'),
-    path('admin/', admin.site.urls),
     path('summernote/', include('django_summernote.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 handler404 = 'pp4.views.handler404'
