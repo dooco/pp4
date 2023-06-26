14c6266c07eabf46a6587b8fe3fc7a73c4965072
Padraig Dooley <padraigdooley@gmail.com>
2023-06-26 06:45:38 +0100

    Correct EMAIL_HOST_USER

--- pp4/settings.py
+++ pp4/settings.py
@@ -247,6 +247,6 @@ else:
     EMAIL_USE_TLS = True
     EMAIL_PORT = 587
     EMAIL_HOST = 'smtp.gmail.com'
-    EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
+    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
     EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
-    DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_USER')
+    DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
